0fbcce23a27a51b8da64394139c8980b2a05b520
OneNaut <75678116+onenaut@users.noreply.github.com>
2021-01-23 13:00:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require ('discord.js')
-const = Join Support server to test not;
+const = Join Support server to test not ;
 // Nothing Here to Look. 
 //Join support server. 
-//This file is just to Index it in Github JavaScript Section.
+//This file is just to Index
+//in Github JavaScript Section.
 ...
